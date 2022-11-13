cedebd10b5bfd1879d340fb2ee7b26e28ed01a4c
Luca Gennaro <lucagennaro91@gmail.com>
2022-11-13 13:01:12 +0100

    update select.js

--- prossimamente.js
+++ prossimamente.js
@@ -43,19 +43,19 @@ let selector2 = document.getElementById("footerbtn2");
 
 function show1() {
     let selectorList1 = document.getElementById("countryselector");
-    if (selectorList1.style.display === "none") {
-      selectorList1.style.display = "block";
-    } else {
+    if (selectorList1.style.display === "block") {
       selectorList1.style.display = "none";
+    } else {
+      selectorList1.style.display = "block";
     }
   }
 
   function show2() {
     let selectorList2 = document.getElementById("locallanguageselector");
-    if (selectorList2.style.display === "none") {
-      selectorList2.style.display = "block";
-    } else {
+    if (selectorList2.style.display === "block") {
       selectorList2.style.display = "none";
+    } else {
+      selectorList2.style.display = "block";
     }
   }
 
