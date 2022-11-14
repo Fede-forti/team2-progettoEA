296900e3ff857a96663cd940fb51d32f68eea501
Mauro Santulli <mauro.santulli@gmail.com>
2022-11-14 23:58:04 +0100

    build homepage

--- Post-navbar/post-navbar.js
+++ Post-navbar/post-navbar.js
@@ -1,2 +1,12 @@
-const content = document.querySelectorAll(".content-li").forEach((item) => {
-});
+const btn = document.querySelectorAll(".dropbtn");
+const span = document.querySelectorAll("span");
+
+for (let i = 0; i < 5; i++) {
+  btn[i].onmouseover = function rotate() {
+    span[i].style.transform = "rotate(270deg)";
+  };
+
+  btn[i].onmouseout = function rotate() {
+    span[i].style.transform = "rotate(90deg)";
+  };
+}
